a551e331a465bc1610edd5e3f56c215987c28359
Leon Kidd <304552328@qq.com>
2014-03-30 14:33:53 +0800

    apache http proxy examples

---
+++ src/main/java/cn/heroes/ud/Main.java
@@ -0,0 +1,14 @@
+package cn.heroes.ud;
+
+import org.apache.http.HttpHost;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final HttpHost host = new HttpHost("localhost", 80);
+	}
+
+}
