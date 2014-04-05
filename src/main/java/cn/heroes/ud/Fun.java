b7c426b4f9f38ec1fd0155fd89d43d9889975840
Leon <304552328@qq.com>
2014-04-05 20:39:24 +0800

    Temp

---
+++ src/main/java/cn/heroes/ud/Fun.java
@@ -0,0 +1,12 @@
+package cn.heroes.ud;
+
+public class Fun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/main/java/cn/heroes/ud/Main.java
+++ src/main/java/cn/heroes/ud/Main.java
@@ -1,14 +1,20 @@
 package cn.heroes.ud;
 
-import org.apache.http.HttpHost;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-		final HttpHost host = new HttpHost("localhost", 80);
+	public static void main(String[] args) throws Exception {
+//		final HttpHost host = new HttpHost("localhost", 80);
+		ServerSocket ss = new ServerSocket(80);
+		Socket socket = ss.accept();
+		System.out.print(socket);
 	}
 
 }
